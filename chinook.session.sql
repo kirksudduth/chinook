b0990b6915fcd63d33f4c811c688a67a49382fd5
Kirk Sudduth <kirksudduth@gmail.com>
2020-08-30 10:54:02 -0500

    completed the first five questions about different queries to db

--- chinook.session.sql
+++ chinook.session.sql
@@ -6,3 +6,37 @@ SELECT
     c.Country
 from Customer c
 WHERE c.Country != "USA";
+
+-- #2 Get all Brazilian customers
+SELECT
+    c.CustomerId as Customer_ID,
+    c.FirstName,
+    c.LastName,
+    c.Country
+from Customer c
+WHERE c.Country = "Brazil";
+
+-- #3 Get all Brazilian customers with their invoices
+SELECT
+    c.CustomerId as Customer_ID,
+    c.FirstName,
+    c.LastName,
+    i.InvoiceID,
+    i.InvoiceDate,
+    i.BillingCountry
+from Customer c
+    join Invoice i on c.CustomerId = i.CustomerId
+WHERE i.BillingCountry = "Brazil"
+ORDER BY c.LastName;
+
+-- #4 get all employees who are sales agents
+SELECT *
+FROM Employee e
+WHERE e.Title = 'Sales Support Agent';
+
+-- #5 show list of all billing countries from invoice TABLE
+SELECT DISTINCT
+    i.BillingCountry
+FROM Invoice i
+ORDER BY i.BillingCountry;
+
